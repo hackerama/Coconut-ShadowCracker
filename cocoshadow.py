172781db95260cf31c3023699b4b74c92823bda9
dsantos-1 <danilo.marques.santos@usp.br>
2018-11-13 22:06:29 -0200

    Update cocoshadow.py
    - Now the shadow file name and the wordlist name can be passed as command line parameters;
    - Now multiple wordlists can be passed as a command line parameter;
    - Added a structure that organizes the command line parameters;
    - Enhanced the help menu;
    - Commented the code.

--- cocoshadow.py
+++ cocoshadow.py
@@ -1,57 +1,82 @@
 #!/usr/bin/python
-#-*-coding:utf:8-*-
-#Coconut Motherfucker Shadow Cracker
-#Version: 1.0
-#coded by: Carlos Néri Correia
+# Coconut Motherfucker Shadow Cracker
+# Version: 1.1
+# coded by: Carlos Néri Correia
+# additional code and comments by: Danilo Santos
+# Implemented and tested using python 3.6.7rc1 on a Linux environment
 
 import crypt
-import sys
+import sys, argparse
+import os.path
 
-print '''
-        +------------------------------------------------------+
-        |   + + +  Coconut Motherfucker Shadow Cracker + + +   |
-        |             coded by: Carlos Néri correia            | 
-        |                  github.com/hackerama                |
-        +------------------------------------------------------+
-'''
+print('''
++------------------------------------------------------+
+|   + + +  Coconut Motherfucker Shadow Cracker + + +   |
+|             coded by: Carlos Néri correia            |
+|    additional code and comments by: Danilo Santos    |
+|                  github.com/hackerama                |
+|                  github.com/dsantos-1                |
++------------------------------------------------------+
+''')
+
+# Checks if a line is syntactically correct
 def find(line):
     for l in line:
-        if l == ("$"):
+        if l==('$'):
             return True
     return False
 
-
+# Gets the hashed password and its salt value from a valid line
 def parse(line):
     global parse1, parse2, salt
-    parse1 = line.split(':')
-    #print parse1[1]
-    parse2 = parse1[1].split('$')
-    salt = "$" + parse2[1] + "$" + parse2[2] + "$"
-    return 0; 
+    parse1=line.split(':')
+    parse2=parse1[1].split('$')
+    salt='$' + parse2[1] + '$' + parse2[2] + '$'
+    return 0
 
+# Uses the wordlists passed as a parameter to attempt to crack the hashed password
+def brute(args):
+    for f in args.wlist.split(','):
+        if(os.path.exists(f)):
+            with open(f, 'r') as brute: 
+                print('--- Current wordlist: %s ---' % f)
 
-def brute():
-    with open ('wlist.txt', 'rb') as brute: 
-        for senha in brute:
-            senha = senha.rstrip()
-            r = crypt.crypt(senha, salt)
-            if r == parse1[1]:
-                print '''
-            [+] Senha encontrada:
-                Usuario: '''+parse1[0]+'''
-                Senha: '''+senha
+                for senha in brute:
+                    senha=senha.rstrip()
+                    r=crypt.crypt(senha, salt)
+                    if(args.verbosity):
+                        print('Attempting %s as password... ' % (senha), end=''),
+                    if r==parse1[1]:
+                        if(args.verbosity):
+                            print('success.\n')
+                        print('User: '+parse1[0]+'\nPassword: '+senha)
+
+                        return
+                    else:
+                        if(args.verbosity):
+                            print('failure.')
+                print('')
+        else:
+            sys.exit('Error: the file %s was not found' % f)
 
 def main():
-    if len(sys.argv) < 2 or sys.argv[1] == "-h":
-        print'''
-                    USAGE cocoshadow.py [SHADOWFILE]
-            '''
-        sys.exit(0)
-    with open (sys.argv[1], 'rb') as string:
-        for line in string: 
-            if find(line) is True:
-                parse(line)
-                brute()
+    # Structure that organizes the command line parameters
+    parser=argparse.ArgumentParser(description='This tool attempts to crack a hashed Unix password, given a shadow file and a list of password files in plain text.')
+    parser.add_argument('shadow_file', help='shadow file\'s name')
+    parser.add_argument('wlist', help='wordlist or list of wordlists in plain text. The filenames must be separated by commas and must not contain spaces. Example:\nwordlist1,wordlist2,...,wordlistn')
+    parser.add_argument('-v', '--verbosity', help='increase output verbosity', action='store_true')
 
-if __name__ == "__main__":
+    args=parser.parse_args()
+	
+    # Checks if the shadow file exists
+    if(os.path.exists(args.shadow_file)):
+        with open(args.shadow_file, 'r') as string:
+            for line in string:
+                if(find(line) is True):
+                    parse(line)
+                    brute(args)
+    else:
+        sys.exit('Error: the shadow file was not found')
+
+if __name__=='__main__':
     main()
